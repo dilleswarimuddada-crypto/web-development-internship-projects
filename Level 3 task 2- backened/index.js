cc17e567a04e61c831cb1413f35d6e62829494fa
dilleswarimuddada-crypto <dilleswarimuddada@gmail.com>
2026-05-04 14:02:48 +0530

    Create index.js

---
+++ Level 3 task 2- backened/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === "admin" && password === "1234") {
+    res.json({ message: "Login successful" });
+  } else {
+    res.json({ message: "Invalid credentials" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
